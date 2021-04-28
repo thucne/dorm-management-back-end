65b101b1c0d80d8d9388d2e45e7087e0f4c88a41
ThanhTam0908 <53783565+ThanhTam0908@users.noreply.github.com>
2021-04-28 12:29:24 +0700

    tamupdate

--- index.js
+++ index.js
@@ -1,3 +1,73 @@
-//hello
+  import express from 'express';
+  // import bodyParser from 'body-parser';
+  import mongoose from 'mongoose';
+  import cors from 'cors';
+  import dotenv from 'dotenv';
+  import cloudinary from 'cloudinary';
+  import Student from './models/student.js';
 
-//hello 2
+  const app = express();
+
+
+  dotenv.config();
+
+  app.use(express.json({ limit: "150mb", extended: true }));
+  app.use(express.urlencoded({ limit: "150mb", extended: true }));
+  app.use(cors());
+
+  app.get('/', async(req, res) => {
+      try {
+          //   const newStudent = {
+          //       full_name: "Bibbie Wrathmall",
+          //       identity_card: 892168756,
+          //       dob: "3/4/2000",
+          //       gender: "Male",
+          //       academic_year: 3,
+          //       field_of_major: "EE",
+          //       folk: "Japanese",
+          //       religion: "Catholicism",
+          //       country: "Indonesia",
+          //       insurance: {
+          //           insurance_number: "SV8580003567330",
+          //           dateofinsurance: "5/4/2021",
+          //           validfrom: "8/4/2021",
+          //           validto: "5/4/2022"
+          //       },
+          //       parentinfo: {
+          //           parentname: "Ashlen",
+          //           gender: "Male",
+          //           address: "3748 Hallows Alley",
+          //           phone: "+62 482 858 2410"
+          //       },
+          //       residentinfo: {
+          //           provincecity: "Ho Chi Minh city",
+          //           district: "Thu Duc",
+          //           ward: "Linh Trung",
+          //           email: "BibbieWrathmall@taobao.com",
+          //           password: "XfBm2H",
+          //           tel: "+44 304 633 1041"
+          //       },
+          //       stayindorm: {
+          //           semester: "II",
+          //           from: "1/6/2021",
+          //           to: "1/1/2022",
+          //           note: null
+          //       },
+          //   }
+          //   const allData = new Student({...newStudent });
+          //   await allData.save();
+          const allData = await Student.find();
+          res.status(200).json(allData);
+      } catch (error) {
+          res.status(404).json({ message: error.message })
+      }
+
+  });
+
+  const PORT = process.env.PORT || 5000;
+
+  mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+      .catch((error) => console.log(error.message));
+
+  mongoose.set('useFindAndModify', false);
