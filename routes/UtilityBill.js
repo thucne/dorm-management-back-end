b06362e21b2dad527c3366679958ac7c9047da1c
ThanhTam0908 <53783565+ThanhTam0908@users.noreply.github.com>
2021-05-24 15:31:01 +0700

    k19

--- routes/UtilityBill.js
+++ routes/UtilityBill.js
@@ -3,13 +3,13 @@ const { requireadminLogin, requirestudentLogin } = require('../controllers/auth'
 const { addUtilitybill, getUtilityById, deletebill, updatePaymentBill, showNonPaidBill, DeleteUtilityWithDate } = require('../controllers/UtilityBill')
 const router = express.Router()
 
-router.post('/addUtilityBill', addUtilitybill);
+router.post('/addUtilityBill', requireadminLogin, addUtilitybill);
 router.get('/getUtilityById/:_id', getUtilityById);
 router.delete('/deletebill/:_id', requireadminLogin, deletebill);
 router.put('/updatePaymentBill/:_id', requireadminLogin, updatePaymentBill);
 // router.post('/getUtilityByRoom', getUtilityByRoom);
 router.get('/showNonPaidBill', showNonPaidBill);
-router.delete('/deleteUtilityWithDate', DeleteUtilityWithDate);
+router.delete('/deleteUtilityWithDate', requireadminLogin, DeleteUtilityWithDate);
 
 
 module.exports = router
